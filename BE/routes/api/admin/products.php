5a5e717e1dedfc14af89968c4c6fb5464cc4efac
hoang150704 <phuongminhhoang77@gmail.com>
2025-02-19 15:12:08 +0700

    Update api

--- BE/routes/api/admin/products.php
+++ BE/routes/api/admin/products.php
@@ -15,9 +15,10 @@ Route::prefix('products')->group(function () {
         Route::post('/', [ProductVariationController::class, 'store']); 
         Route::get('/{id}', [ProductVariationController::class, 'show']);
         Route::put('/{id}', [ProductVariationController::class, 'update']);
-        Route::delêt('/{id}', [ProductVariationController::class, 'destroy']);
+        
     });
     Route::prefix('{idProduct}/attributes')->group(function () {
         Route::get('/', [ProductAttributeController::class, 'index']); 
     });
 });
+Route::delete('variants/{id}', [ProductVariationController::class, 'destroy']);
