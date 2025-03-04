62ff3cef6e874db656e80f97f66cbf8483013a09
hoang150704 <phuongminhhoang77@gmail.com>
2025-03-04 13:58:50 +0700

    FIx bug seeder

--- BE/database/seeders/ProductSeeder.php
+++ BE/database/seeders/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
     {
         //
         DB::table('products')->insert([
-            ['name' => "Áo Thun Nam Họa Tiết In Excursion Mighty Bear Form Regular", 'description' => "Áo thun xịn của mình",'weight'=>500 ,'short_description' => 'Áo thun xịn của mình', 'main_image' => 2, 'slug' => 'ao-thun-nam-hoa-tiet-in-excursion-mighty-bear-form-regular'],
+            ['name' => "Áo Thun Nam Họa Tiết In Excursion Mighty Bear Form Regular", 'description' => "Áo thun xịn của mình" ,'short_description' => 'Áo thun xịn của mình', 'main_image' => 2, 'slug' => 'ao-thun-nam-hoa-tiet-in-excursion-mighty-bear-form-regular'],
         ]);
     }
 }
