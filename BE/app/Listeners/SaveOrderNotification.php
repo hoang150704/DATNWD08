e744a58b7a783fc0f96b38fbf8afd41a1fe0c1e8
tuduong212 <dtu52717@gmail.com>
2025-03-26 21:52:21 +0700

    Realtime voucher quantity

--- BE/app/Listeners/SaveOrderNotification.php
+++ BE/app/Listeners/SaveOrderNotification.php
@@ -29,5 +29,14 @@ class SaveOrderNotification
             'message' => 'Khách hàng ' . $event->order->o_name . ' vừa đặt đơn hàng trị giá ' . $event->order->final_amount . 'đ' . ' bằng phương thức ' . $event->order->payment_method,
             'created_at' => $event->order->created_at
         ]);
+        if ($event->voucher) {
+            if ($event->voucher->usage_limit == 0) {
+                Notification::create([
+                    'title' => 'Voucher hết lượt dùng: ' . $event->voucher->code,
+                    'message' => 'Voucher ' . $event->voucher->code . ' đã hết lượt dùng',
+                    'created_at' => $event->order->created_at
+                ]);
+            }
+        }
     }
 }
