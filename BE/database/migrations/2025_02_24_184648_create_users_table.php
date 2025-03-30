e82c07a65e470fe6319a1679981f02ea7f6251e4
Lamnamday <chinh.nncn@gmail.com>
2025-03-30 21:58:42 +0700

    Xóa dòng migration user

--- BE/database/migrations/2025_02_24_184648_create_users_table.php
+++ BE/database/migrations/2025_02_24_184648_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-
             $table->string('name',255);
             $table->string('username', 50)->unique()->index();
             $table->string('email')->unique()->index();
