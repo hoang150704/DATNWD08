d7e0eee1fabddbe4a0827a66c6c6e12bdc94f30b
gitlovehub <hungtd010302@gmail.com>
2025-03-31 21:10:11 +0700

    Update ContactController.php
    - Xóa hàm update liên hệ

--- BE/app/Http/Controllers/Api/Admin/ContactController.php
+++ BE/app/Http/Controllers/Api/Admin/ContactController.php
@@ -42,14 +42,6 @@ class ContactController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
