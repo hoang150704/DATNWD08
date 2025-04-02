51bac4d39f2c3e20db423a7c7e9f911f8a234b0c
gitlovehub <hungtd010302@gmail.com>
2025-04-02 17:07:42 +0700

    Update Contact.php
    - $fillable thêm user_id

--- BE/app/Models/Contact.php
+++ BE/app/Models/Contact.php
@@ -4,12 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name', 'email', 'phone', 'message', 'status'];
+    protected $fillable = ['name', 'email', 'phone', 'message', 'status', 'user_id'];
 
     protected $dates = ['deleted_at']; // Đánh dấu ngày xóa mềm
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
