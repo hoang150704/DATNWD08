6b48f191a285be37270f77753b4dd1fe41a86baa
gitlovehub <hungtd010302@gmail.com>
2025-04-03 15:23:02 +0700

    Update contact.php

--- BE/routes/api/admin/contact.php
+++ BE/routes/api/admin/contact.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('contact')->group(function () {
     Route::get('/', [ContactController::class, 'index']);
-    Route::post('/create', [ContactController::class, 'store']);
     Route::post('/reply_mail', [ContactController::class, 'reply_mail']);
     Route::get('/{id}', [ContactController::class, 'show']);
-    Route::delete('/', [ContactController::class, 'destroy']);
+    Route::delete('/{id}', [ContactController::class, 'destroy']);
+    Route::put('/contacts/{id}/start-processing', [ContactController::class, 'startProcessing']);
 });
