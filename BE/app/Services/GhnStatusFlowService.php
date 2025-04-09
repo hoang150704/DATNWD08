a40c7635e71334f5a36ca67dc4b778ccd5f43b1a
hoang150704 <phuongminhhoang77@gmail.com>
2025-04-09 13:32:08 +0700

    Update

--- BE/app/Services/GhnStatusFlowService.php
+++ BE/app/Services/GhnStatusFlowService.php
@@ -7,27 +7,21 @@ class GhnStatusFlowService
     public static array $statusFlow = [
         // Trạng thái bắt đầu
         'ready_to_pick' => ['picking', 'cancel'],
-
         // Đang lấy hàng
         'picking' => ['money_collect_picking', 'picked', 'cancel'],
         'money_collect_picking' => ['picked'],
-
         // Hàng đã lấy
         'picked' => ['storing', 'sorting', 'transporting', 'delivering', 'delivery_fail', 'damaged', 'lost'],
-
-        // Vận chuyển / kho
+        // Vận chuyển / kho  
         'sorting' => ['transporting'],
         'storing' => ['transporting', 'sorting', 'delivering', 'delivery_fail', 'damaged', 'lost'],
         'transporting' => ['storing', 'delivering', 'delivery_fail', 'damaged', 'lost'],
-
         // Giao hàng
         'delivering' => ['money_collect_delivering', 'delivered', 'delivery_fail', 'damaged', 'lost'],
         'money_collect_delivering' => ['delivered'],
         'delivered' => [],
-
         // Giao thất bại
-        'delivery_fail' => ['waiting_to_return', 'return', 'damaged', 'lost'],
-
+        'delivery_fail' => ['waiting_to_return', 'storing','return', 'damaged', 'lost'],
         // Trả hàng
         'waiting_to_return' => ['return'],
         'return' => ['return_sorting', 'return_transporting', 'damaged', 'lost'],
@@ -36,11 +30,9 @@ class GhnStatusFlowService
         'returning' => ['returned', 'return_fail', 'damaged', 'lost'],
         'return_fail' => ['return', 'damaged', 'lost'],
         'returned' => [],
-
         // Ngoại lệ
         'exception' => [],
-
-        // Hủy, hư, mất
+        // Hủy, hư, mấ
         'cancel' => [],
         'damaged' => [],
         'lost' => [],
